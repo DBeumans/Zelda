f10bb13414e4ad78a0c0f5c57e13e68e79997f13
Rens <r.walstijn@live.nl>
2017-01-10 10:17:56 +0100

    Movement added

--- Assets/Scripts/GameBehaviour/InputBehaviour.cs
+++ Assets/Scripts/GameBehaviour/InputBehaviour.cs
@@ -2,6 +2,4 @@
 using System.Collections;
 
 public class InputBehaviour : MonoBehaviour {
-
-
 }
--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class Player_Movement : MonoBehaviour {
+    void FixedUpdate() {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
 
+        Vector3 direction = new Vector3(x, 0f, z);
+
+        if (direction.magnitude > 1) {
+            direction.Normalize();
+        }
+        transform.Translate(direction);
+    }
 }
-
