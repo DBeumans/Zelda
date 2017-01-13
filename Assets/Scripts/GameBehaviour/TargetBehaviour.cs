88db8fde05ca779d510c23f58dfa1fc76f27978c
Rens <r.walstijn@live.nl>
2017-01-13 13:16:27 +0100

    Target added and working

---
+++ Assets/Scripts/GameBehaviour/TargetBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetBehaviour : MonoBehaviour {
+	private GameObject _camera;
+
+	void Start () {
+		_camera = GameObject.FindWithTag("MainCamera");
+	}
+	void Update () {
+		transform.LookAt(_camera.transform);
+	}
+}
