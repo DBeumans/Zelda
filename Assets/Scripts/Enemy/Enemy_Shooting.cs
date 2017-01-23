19602ea65e14c4b3594beac0bce9bf112fee59c5
DBeumans <buncho.gamingnetwork@gmail.com>
2017-01-23 12:02:19 +0100

    Added enemyr_shooting script with one function

---
+++ Assets/Scripts/Enemy/Enemy_Shooting.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Shooting : MonoBehaviour {
+
+    [SerializeField]
+    GameObject _bullet;
+
+    float _time = 10;
+    float _maxTime = 10;
+
+    /*
+        Wat moet ik schieten ?
+        Wanneer mag ik schieten ? {
+            ik mag alleen schieten als er geen kogels in het veld zijn.
+        }
+        
+    */
+    
+
+    void Update()
+    {
+        _time-=.05f;
+        if(_time <=0)
+        {
+            _time = _maxTime;
+            Shoot();
+        }
+    }
+    
+    public void Shoot()
+    {
+        // mag alleen schieten als er geen kogels in het veld zijn.
+        
+        GameObject go = Instantiate(_bullet);
+        go.transform.position = this.gameObject.transform.position;
+        Debug.Log("Enemy Shoot");
+    }
+    
+}
