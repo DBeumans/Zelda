171441cc9fa29717b0bf1ead3c24da36eea69187
Rens <r.walstijn@live.nl>
2017-01-26 12:21:27 +0100

    add shoot movement

---
+++ Assets/Scripts/Enemy/Shoot_Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Shoot_Movement : MonoBehaviour {
+	private GameObject _camera;
+	private GameObject _player;
+	private Vector3 _target;
+	
+	void Start() 
+    {
+		_camera = GameObject.FindWithTag("MainCamera");
+		_player = GameObject.FindWithTag("Player");
+		_target = _player.transform.position;
+	}
+	void Update()
+    {
+        transform.LookAt(_target);
+        transform.position += GetForward() / 50;
+    }
+
+    private Vector3 GetForward()
+    {
+        return transform.forward;
+    }
+}
