cf69208706b93e0f7205a383e81c8be696c968a9
DBeumans <buncho.gamingnetwork@gmail.com>
2017-01-27 12:27:57 +0100

    fixed player camera

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -3,14 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CameraMovement : MonoBehaviour
-{
-    
-    Camera _camera;
-    [SerializeField]
-    bool _smooth;
-    [SerializeField]
-    float _smoothSpeed = .1f;
-    
+{ 
+    Camera _camera;    
     [SerializeField]
     private GameObject[] playerPos;
 
@@ -18,82 +12,53 @@ public class CameraMovement : MonoBehaviour
 
     [SerializeField]
     float _angleX, _angleY, _angleZ;
-    [SerializeField] float _default_angleY;
     
     [SerializeField]
     float _posX, _posY, _posZ;
 
     [SerializeField]
-    float _fov; // camera field of view.
-    [SerializeField]
     bool _toggleTargetView = false;
+
+    [SerializeField]
+    float _lookDistance;
     
     void Start()
     {
         _camera = GetComponent<Camera>();
-        StartCoroutine(CameraFieldOfView());
-        StartCoroutine(ZoomInOut());
 
         _offset = new Vector3(_posX, _posY, _posZ);
     }
 
     void FixedUpdate()
     {
-     
-        Vector3 _desiredPosition = playerPos[0].transform.position + _offset;
-        if(_smooth)
+        if (_toggleTargetView)
         {
-            transform.position = Vector3.Lerp(transform.position, _desiredPosition, _smoothSpeed);
+            if (playerPos[0] != null && playerPos[1] != null)
+            {
+
+                Vector3 lookPoint = Vector3.Lerp(playerPos[0].transform.position, playerPos[1].transform.position, 0.5f);
+
+                float distance = Vector3.Distance(playerPos[0].transform.position, playerPos[1].transform.position);
+
+                if (distance < _lookDistance)
+                {
+                    _camera.transform.LookAt(lookPoint);
+                }
+                else
+                {
+                    this.transform.localEulerAngles = new Vector3(_angleX, _angleY, _angleZ);
+                }
+
+            }
         }
         else
         {
-            transform.position = _desiredPosition;
+            this.transform.position = _offset;
+            transform.localEulerAngles = new Vector3(_angleX, _angleY, _angleZ);
         }
-        
-    }
 
-    IEnumerator CameraFieldOfView()
-    {
-        while (true)
-        {
-            _camera.fieldOfView = _fov;
-            yield return null;
-        }
-    }
-
-
-    IEnumerator ZoomInOut()
-    {
-        while (true)
-        {
-            if(_toggleTargetView)
-            {
-                if (playerPos[0] != null && playerPos[1] != null)
-                {
-
-                    Vector3 lookPoint = Vector3.Lerp(playerPos[0].transform.position, playerPos[1].transform.position, 0.5f);
-
-                    Vector3 _desiredPosition = transform.position + _offset;
-
-                    float distance = Vector3.Distance(playerPos[0].transform.position, playerPos[1].transform.position);
-
-                    if (distance < 10)
-                    {
-                        _camera.transform.LookAt(lookPoint);
-                    }
-                    else
-                    {
-                        _camera.transform.LookAt((playerPos[0].transform.position));
-                    }
-                   
-                }
-            }
-            else
-            {
-                this.transform.position = _offset;
-                this.transform.localEulerAngles = new Vector3(_angleX, _angleY, _angleZ);
-            }
-            yield return new WaitForSeconds(0.02f);
-        }
+        if (this.transform.localEulerAngles.y < -30 || this.transform.localEulerAngles.y > 30)
+            this.transform.localEulerAngles = new Vector3(_angleX, _angleY, _angleZ);
+       
     }
 }
--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -5,15 +5,19 @@ public class Player_Movement : InputBehaviour {
 
     [SerializeField]
     float _movementSpeed = 20f;
+    [SerializeField]
+    float _rotationSpeed = 2f;
     void Update() {
         KeysCheck();
-        Vector3 direction = new Vector3(_x, 0f, _z);
-
-        if (direction.magnitude > 1) {
-            direction.Normalize();
+        Vector3 _angle = new Vector3(0f, _x, 0f);
+        Vector3 _direction = new Vector3(0f, 0f, _z);
+        if (_direction.magnitude > 1) {
+            _direction.Normalize();
         }
-        transform.Translate(direction * _movementSpeed * Time.deltaTime);
+        transform.Translate(_direction * _movementSpeed * Time.deltaTime);
 
-        
+        this.transform.Rotate(_angle*_rotationSpeed);
     }
+
+
 }
