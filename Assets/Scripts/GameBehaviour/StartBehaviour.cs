90bb8f637b665a8eac37a0f229eec02d90c11ec4
Rens <r.walstijn@live.nl>
2017-01-30 21:32:55 +0100

    Start screen added

---
+++ Assets/Scripts/GameBehaviour/StartBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class StartBehaviour : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			 Application.LoadLevel(1);
+		}
+	}
+}
